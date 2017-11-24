f8f4e5c0e1888b1d19a9d8ca1485e5629d4f603a
Your Name <robpeterman573@gmail.com>
2017-11-24 13:52:18 -0500

    initial commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(
   })
 );
 
-app.get('/api/*', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ok: true});
 });
 
--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
+app.get('/map', ()=>{
+
+});
+
 module.exports = {app};
+
